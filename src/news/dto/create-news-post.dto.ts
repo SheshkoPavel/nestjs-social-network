2aaaabd13381645cbf5ae7a007fe1286fa67857f
Sheshko Pavel <derzurb@mail.ru>
2022-04-04 23:44:40 +0300

    docker-compose build docker-compose up

--- src/news/dto/create-news-post.dto.ts
+++ src/news/dto/create-news-post.dto.ts
@@ -3,6 +3,6 @@ import {ApiProperty} from "@nestjs/swagger";
 export class CreateNewsPostDto {
     @ApiProperty({example: 'Текст новости', description: 'Текст новости'})
     readonly newsText: string;
-    @ApiProperty({example: 'Адрес картинки', description: 'URL картинки'})
+    @ApiProperty({example: 'https://www.images/news-images-3.jpg', description: 'URL картинки'})
     readonly imageURL: string;
 }
