76949ce3d6a8fc971e8bb92727f1c830b12806b5
Sheshko Pavel <derzurb@mail.ru>
2022-04-25 11:12:28 +0300

    final build for showing

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import {NestFactory} from "@nestjs/core";
 import {AppModule} from "./app.module";
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
+//Final build before showing
+
 const cors = require('cors');
 
 async function start() {
--- src/news/news.module.ts
+++ src/news/news.module.ts
@@ -4,7 +4,7 @@ import { NewsService } from './news.service';
 import {SequelizeModule} from "@nestjs/sequelize";
 import {NewsPost} from "./news.model";
 
-//Это модуль, который регистрируется в app.module
+//Это модуль, который регистрируется в app.module и содержит описание, какие контроллеры и сервисы используются
 
 @Module({
   controllers: [NewsController],
